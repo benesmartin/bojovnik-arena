b44cf0b453f4984f9ef847a5ba37e2961eb5ebfa
benesmartin <93920137+benesmartin@users.noreply.github.com>
2021-12-11 16:48:21 +0100

    add mage class

---
+++ bojovnik-arena/bojovnik-arena/Mage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bojovnik_arena
+{
+    public class Mage : Bojovnik
+    {
+        public Mage(int hp, int dmg, int dodgechance, int blockchance) : base(hp, dmg, dodgechance, blockchance)
+        {
+        }
+    }
+}
