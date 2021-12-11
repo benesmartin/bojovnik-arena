aa53f86b4a3f0f79334cda011d696e74f547a2b1
benesmartin <93920137+benesmartin@users.noreply.github.com>
2021-12-11 16:34:37 +0100

    add bojovnik base class

---
+++ bojovnik-arena/bojovnik-arena/Bojovnik.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bojovnik_arena
+{
+    abstract class Bojovnik
+    {
+        public int HP { get; set; }
+        public int DMG { get; set; }
+        public int DodgeChance { get; set; }
+        public int BlockChance { get; set; }
+    }
+}
