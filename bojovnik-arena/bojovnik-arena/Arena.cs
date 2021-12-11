cd6e36cbbfa8aacd60af6a18a7a412a25efae7e6
benesmartin <93920137+benesmartin@users.noreply.github.com>
2021-12-11 18:13:26 +0100

    add arena class with all warriors output

---
+++ bojovnik-arena/bojovnik-arena/Arena.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bojovnik_arena
+{
+    public class Arena
+    {
+        public void Turnaj(List<Bojovnik> warriors)
+        {
+            Extensions.Shuffle(warriors);
+            var warriorsCount = warriors.Count();
+            for (int i = 0; i < warriorsCount; i++)
+            {
+                Console.WriteLine(i+1 + ". bojovník je: " + warriors[i].Name);
+            }
+        }
+    }
+}
