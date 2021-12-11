4dfff98eacf86d5f8b4e3228f3ee5c9197e73d48
benesmartin <93920137+benesmartin@users.noreply.github.com>
2021-12-11 16:34:14 +0100

    add vs project

---
+++ bojovnik-arena/bojovnik-arena/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bojovnik_arena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
