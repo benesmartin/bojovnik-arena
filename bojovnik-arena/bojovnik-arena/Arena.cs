0ab25330ada0c86243c09188a90c5430acc67887
benesmartin <93920137+benesmartin@users.noreply.github.com>
2022-01-02 17:58:18 +0100

    add gamemodes
    again, a longer commit

--- bojovnik-arena/bojovnik-arena/Arena.cs
+++ bojovnik-arena/bojovnik-arena/Arena.cs
@@ -10,7 +10,7 @@ namespace bojovnik_arena
 {
     public class Arena
     {
-        private int pocetBoj; private static Random rng = new Random(); private Bojovnik bojovnik; bool game, main;
+        private int pocetBoj, HP, DMG, chance; private static Random rng = new Random(); private Bojovnik bojovnik; bool game, main; char rezim, vyber;
         private static string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); //zjisti cestu na plochu
         private static string filepath = path + "\\bojovnik-arena.txt"; //prida do cesty nazev textoveho souboru
         public void Turnaj(List<Bojovnik> warriors)
@@ -77,22 +77,102 @@ namespace bojovnik_arena
                 if (pocetBoj == 2 || pocetBoj == 4 || pocetBoj == 8 || pocetBoj == 16)
                     break;
             }
-            for (int i = 0; i < pocetBoj; i++)
+            while (true)
             {
-                var randomHP = rng.Next(50, 101);
-                var randomDMG = rng.Next(25, 51);
-                var randomChance = rng.Next(10, 51);
-                var randomChoice = rng.Next(0, 4);
-                if (randomChoice == 0)
-                    bojovnik = new Berserker(randomHP, randomDMG, 0, 0, randomChance, "Berserker [" + (i + 1) + "]");
-                if (randomChoice == 1)
-                    bojovnik = new Mage(randomHP, randomDMG, 0, 0, 0, "Mág [" + (i + 1) + "]");
-                if (randomChoice == 2)
-                    bojovnik = new Warrior(randomHP, randomDMG, 0, randomChance, 0, "Bojovník [" + (i + 1) + "]");
-                if (randomChoice == 3)
-                    bojovnik = new Archer(randomHP, randomDMG, randomChance, 0, 0, "Lukostřelec [" + (i + 1) + "]");
-                bojovnici.Add(bojovnik);
+                Console.Clear();
+                Console.Write("Vyber si režim:\n\t> m - manuální (vše si naklikám sám)\n\t> p - poloautomatický (chci náhodné vlastnosti bojovníků, ale průběhy kol mě zajímají)\n\t> a - automatický (zajímají mě jen stručné statistiky turnaje)\n> ");
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                rezim = Console.ReadKey().KeyChar;
+                Console.ResetColor();
+                if (rezim == 'm' || rezim == 'p' || rezim == 'a')
+                    break;
             }
+            if(rezim == 'p' || rezim == 'a')
+            {
+                Console.Clear();
+                for (int i = 0; i < pocetBoj; i++)
+                {
+                    var randomHP = rng.Next(50, 101);
+                    var randomDMG = rng.Next(25, 51);
+                    var randomChance = rng.Next(10, 51);
+                    var randomChoice = rng.Next(0, 4);
+                    if (randomChoice == 0)
+                        bojovnik = new Berserker(randomHP, randomDMG, 0, 0, randomChance, "Berserker [" + (i + 1) + "]");
+                    if (randomChoice == 1)
+                        bojovnik = new Mage(randomHP, randomDMG, 0, 0, 0, "Mág [" + (i + 1) + "]");
+                    if (randomChoice == 2)
+                        bojovnik = new Warrior(randomHP, randomDMG, 0, randomChance, 0, "Bojovník [" + (i + 1) + "]");
+                    if (randomChoice == 3)
+                        bojovnik = new Archer(randomHP, randomDMG, randomChance, 0, 0, "Lukostřelec [" + (i + 1) + "]");
+                    bojovnici.Add(bojovnik);
+                }  
+            }
+            else if(rezim == 'm')
+            {
+                Console.Clear();
+                for (int i = 0; i < pocetBoj; i++)
+                {
+                    while (true)
+                    {
+                        Console.Clear();
+                        Console.Write("Jaký bude bojovník č." + (i + 1) + " typ?\n\t> b - Berserker (šance provést útok 2x)\n\t> m - Mág (je cool)\n\t> w - Bojovník (šance zablokovat útok)\n\t> a - Lukostřelec (šance vyhnout se útoku)\n> ");
+                        vyber = Console.ReadKey().KeyChar;
+                        if (vyber == 'b' || vyber == 'm' || vyber == 'w' || vyber == 'a')
+                            break;
+                    }
+
+                    Console.Clear();
+                    Console.Write("Jaké bude jeho jméno?\n> ");
+                    var jmeno = Console.ReadLine();
+
+                    while (true)
+                    {
+                        Console.Clear();
+                        Console.Write("Kolik bude mít " + jmeno + " životů? (50-100)\n> ");
+                        var sHP = Console.ReadLine();
+                        HP = Convert.ToInt32(sHP);
+                        if (HP >= 50 && HP <= 100)
+                            break;
+                    }
+                    while (true)
+                    {
+                        Console.Clear();
+                        Console.Write("Jak silný bude " + jmeno + "? (25-50)\n> ");
+                        var sDMG = Console.ReadLine();
+                        DMG = Convert.ToInt32(sDMG);
+                        if (DMG >= 25 && DMG <= 50)
+                            break;
+                    } 
+
+                    if (vyber == 'b' || vyber == 'w' || vyber == 'a')
+                    {
+                        while (true)
+                        {
+                            Console.Clear();
+                            if (vyber == 'b')
+                                Console.Write("Jakou šanci bude mít " + jmeno + " na dvojnásobný útok? (10-50)\n> ");
+                            else if (vyber == 'w')
+                                Console.Write("Jakou šanci bude mít " + jmeno + " na zablokování útoku? (10-50)\n> ");
+                            else if (vyber == 'a')
+                                Console.Write("Jakou šanci bude mít " + jmeno + " na vyhnutí se útoku? (10-50)\n> ");
+                            var sChance = Console.ReadLine();
+                            chance = Convert.ToInt32(sChance);
+                            if (chance >= 10 && chance <= 50)
+                                break;
+                        }
+                    }
+   
+                    if (vyber == 'b')
+                        bojovnik = new Berserker(HP, DMG, 0, 0, chance, jmeno);
+                    if (vyber == 'm')
+                        bojovnik = new Mage(HP, DMG, 0, 0, 0, jmeno);
+                    if (vyber == 'w')
+                        bojovnik = new Warrior(HP, DMG, 0, chance, 0, jmeno);
+                    if (vyber == 'a')
+                        bojovnik = new Archer(HP, DMG, chance, 0, 0, jmeno);
+                    bojovnici.Add(bojovnik);
+                }
+            }            
             return bojovnici;
         }
         public void PrintWarriors(List<Bojovnik> warriors)
@@ -176,7 +256,8 @@ namespace bojovnik_arena
             Console.WriteLine("\nStiskni libovolnou klávesu pro pokračování...");
             Console.Write("--------------------------------------------------\n");
                 File.AppendAllText(filepath, warriors[0].Name + " vs " + warriors[1].Name);
-            Console.ReadLine();
+            if(rezim != 'a')
+                Console.ReadLine();
             var w0hp = warriors[0].HP;
             var w1hp = warriors[1].HP;
             while (warriors[0].isAlive() && warriors[1].isAlive())
@@ -193,7 +274,8 @@ namespace bojovnik_arena
                 Console.ResetColor();
                 Console.WriteLine(" / " + w1hp);
                 Console.WriteLine("--------------------------------------------------");
-                Thread.Sleep(1000);   
+                if(rezim != 'a')
+                    Thread.Sleep(1000);   
                 if (warriors[1].isAlive())
                 {
                     Console.Clear();
@@ -208,7 +290,8 @@ namespace bojovnik_arena
                     Console.WriteLine(" / " + w0hp);
                     Console.WriteLine("HP " + warriors[1].Name + ": " + warriors[1].HP + " / " + w1hp);
                     Console.WriteLine("--------------------------------------------------");
-                    Thread.Sleep(1000);
+                    if (rezim != 'a')
+                        Thread.Sleep(1000);
                 }
             }
             if (warriors[0].isAlive())
@@ -234,8 +317,9 @@ namespace bojovnik_arena
                 warriors.Remove(item);
             }
             warriorsD.Clear();
-            Console.Write("--------------------------------------------------");          
-            Console.ReadLine();
+            Console.Write("--------------------------------------------------\nStiskni libovolnou klávesu pro pokračování...\n--------------------------------------------------\n");
+            if (rezim != 'a') 
+                Console.ReadLine();
         }
     }
 }
